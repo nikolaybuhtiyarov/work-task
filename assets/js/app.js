56001874b68a27c66dadada0f5ee72bee656ce6c
nikolaybuhtiyarov <nikolaybuhtiyarov@gmail.com>
2017-04-23 18:10:11 +0300

    Add dialog window

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,18 @@
+$('.cancel').click(function() {
+  $('.dialog').addClass("visible");
+});
 
+
+
+$('.btn-success').on('click',function(event) {
+    $(this).parents('.mainCol').remove();
+    $('.dialog').toggleClass("visible");
+    event.stopPropagation();
+});
+
+
+    
+$('.btn-danger').click(function() {
+    $('.dialog').toggleClass("visible");
+});
 
